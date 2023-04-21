f2603a962f772577f92356bfd664bdc3ed83bbb8
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-04-21 13:26:29 -0700

    Add task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Contains puppet DSL that creates a new file with specifications
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
