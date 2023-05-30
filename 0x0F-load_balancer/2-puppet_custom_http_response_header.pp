457266086160ee9a879d2412900e4eb49c21a245
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-05-30 21:48:19 +0100

    Add task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+include stdlib
+
+
+exec {'install nginx':
+  provider => shell,
+  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Hello World!" | tee /var/www/html/index.html'
+}
+
+file_line {'Redirection line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tserver_name _;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4\$request_uri;\n\t}\n\n\tadd_header X-Served-By ${hostname};\n",
+  match  => "\tserver_name _;"
+}
+
+exec {'restart nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart'
+}
