0726c7d0df4cc2293dc10c7615016c72802ff7cb
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-06 22:20:07 +0100

    Add task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,25 @@
-#!/usr/bin/python3
-"""Use Reddit API to get the top 10 titles of hot posts"""
+#!/bin/usr/python3
+"""Get top ten tiles"""
 import requests
 
 
 def top_ten(subreddit):
-    """Gets top 10 titles of hot post"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User_Agent': 'YourBot/1.0'}
+    headers = {'User-Agent': 'YourBot/1.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    data = response.json()
-    for post in data['data']['children']:
-        print(post['data']['title'])
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                if 'data' in post and 'title' in post['data']:
+                    print(post['data']['title'])
+        else:
+            print("No posts found.")
+    else:
+        print("Invalid subreddit.")
+
+# Example usage
+subreddit = 'python'
+top_ten(subreddit)
