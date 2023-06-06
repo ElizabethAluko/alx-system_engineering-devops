37ecf7bfb939b4362e71327bd4e93e2a5a2b3272
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-06 16:01:46 +0100

    Add task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,9 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User_Agent': 'custom-agent/0.0.1'}
 
-    response = requests.request("GET", url, headers=headers, allow_redirects=False)
+    response = requests.request(
+            "GET", url, headers=headers,
+            allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         if 'data' in data and 'subscribers' in data['data']:
