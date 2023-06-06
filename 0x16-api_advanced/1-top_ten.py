419681d19e2913fe35d7ab42d161eafcaaf5abe8
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-06 20:34:43 +0100

    Add task2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Use Reddit API to get the top 10 titles of hot posts"""
+
+import requests
+
+def top_ten(subreddit):
+    """Gets top 10 titles of hot post"""
--- 0x16-api_advanced/p.py
+++ 0x16-api_advanced/p.py
@@ -1,15 +1,9 @@
 import requests
 
 subreddit = 'ruby'
-url = f"https://www.reddit.com/r/{subreddit}/about.json"
+url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=1"
 headers = {'User_Agent': 'Browser/1.0'}
 
 response = requests.get(url, headers=headers, allow_redirects=False)
-if response.status_code == 200:
-    data = response.json()
-    if 'data' in data and 'subscribers' in data['data']:
-        print(data['data']['subscribers'])
-    else:
-        print(0)
-else:
-    print(0)
+data = response.json()
+print(data)
