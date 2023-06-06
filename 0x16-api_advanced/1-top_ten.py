ba8ca8730bd6e75054bc022b276a4cb931660b8b
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-06 21:29:38 +0100

    Add task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """Use Reddit API to get the top 10 titles of hot posts"""
-
 import requests
+
 
 def top_ten(subreddit):
     """Gets top 10 titles of hot post"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User_Agent': 'YourBot/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                if 'data' in post and 'title' in post['data']:
-                    print(post['data']['title'])
-        else:
-                    print("No posts found.")
-    else:
-        print(None)
+    if response.status_code == 404:
+        print("None")
+        return
+    data = response.json()
+    for post in data['data']['children']:
+        print(post['data']['title'])
