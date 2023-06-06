181e1035d52e70cf49c7b6a7165e09a5cac18014
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-06 21:16:18 +0100

    Add task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Gets top 10 titles of hot post"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User_Agent' 'YourBot/1.0'}
+    headers = {'User_Agent': 'YourBot/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
