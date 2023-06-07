e39535dde0cddc29feb16d235cbdeb1908ddd9b9
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-07 21:47:52 +0100

    Add task

--- 0x16-api_advanced/p.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-subreddit = 'ruby'
-url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=1"
-headers = {'User_Agent': 'Browser/1.0'}
-
-response = requests.get(url, headers=headers, allow_redirects=False)
-data = response.json()
-print(data)
