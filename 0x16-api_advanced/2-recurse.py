fc79c1d0efd65153170eb0b9fecd9c324b4b414b
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-07 21:28:02 +0100

    Add task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,14 +7,17 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0
+        (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url, headers=headers,
+            params=params, allow_redirects=False)
 
     if response.status_code == 404:
         return None
