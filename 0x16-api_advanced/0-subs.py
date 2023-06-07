c5ab35e87297adef12dc5f4e0c6e1d9d69d38a02
ElizabethAluko <jolaoyebethy24@gmail.com>
2023-06-07 20:53:00 +0100

    Add task1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,18 +8,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User_Agent':  '0x16-api_advanced:project:\
-            v1.0.0 (by /u/firdaus_cartoon_jr)'}
-
-    response = requests.get(
-            url, headers=headers,
-            allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        if "data" in data and "subscribers" in data['data']:
-            return data["data"]["subscribers"]
-        else:
-            return 0
-    else:
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
