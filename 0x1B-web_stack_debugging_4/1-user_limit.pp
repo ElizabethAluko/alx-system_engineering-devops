f1929d3b47e6d1ed77c6c95ea84e66ee712f38f7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-06-15 14:32:48 +0100

    Add task1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,32 +1,11 @@
 # Configuration to enable holberton user to login and open files.
 
-# Create the "holberton" user
-user { 'holberton':
-  ensure     => present,
-  managehome => true,
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# Grant necessary permissions
-group { 'www-data':
-  ensure => present,
-}
-
-user { 'holberton':
-  groups => ['www-data'],
-}
-
-# Adjust file permissions
-file { '/etc/nginx':
-  ensure => directory,
-  owner  => 'holberton',
-  group  => 'holberton',
-  mode   => '0755',
-  recurse => true,
-}
-
-# Restart Nginx
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  require => File['/etc/nginx'],
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
