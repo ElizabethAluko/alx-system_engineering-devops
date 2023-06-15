eddf1d791d81903c7233640a07c5b0324620f8f9
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-06-15 13:50:20 +0100

    Add task0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# reconfigure nginx to handle more traffic
+
+exec { 'reconfg-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+#restart nginx to apply the changes
+exec { 'nginx restart',
+  command => 'service nginx restart',
+  path    => '/etc/init.d/"
+}
