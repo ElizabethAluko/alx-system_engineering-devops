d04bf95146d136445082023b266de18d0a277f58
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-06-15 14:36:36 +0100

    Add task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@
 exec { 'reconfg-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
+} ->
 
 #restart nginx to apply the changes
 exec { 'nginx restart',
